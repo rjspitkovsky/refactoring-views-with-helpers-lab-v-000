c613e38027ebbae6456a223b4f30ea3638a0b6b9
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-04-16 18:30:44 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,9 +2,10 @@ class Song < ActiveRecord::Base
   belongs_to :artist
 
   def artist_name
-    self.artist.name 
+    self.artist.name
   end
 
   def artist_name=(name)
+    self.artist.name = name 
   end
 end
